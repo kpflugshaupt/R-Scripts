6d50eb8a00edac181dc64bdf8897117bcf4a3cf5
kpflugshaupt <kpflugshaupt@gmail.com>
2018-09-27 15:02:12 +0200

    Comment it!

--- util.R
+++ util.R
@@ -1,6 +1,10 @@
-# Normalize a vector to unit length
+############################################################################################
+# Miscellaneous helpers
+############################################################################################
 
+
+# Helper function: Calculate vector length
 vlength   <- function(i_vect) { sqrt(sum(i_vect ^ 2)) }
 
+# Normalize a vector to unit length
 normalize <- function(i_vect) { i_vect / vlength(i_vect) }
-
