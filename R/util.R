75ae9e99c2b6bed458814a3674ffa80cbaa47e54
kpflugshaupt <kpflugshaupt@me.com>
2018-09-28 17:40:40 +0200

    Roxygen comments

--- R/util.R
+++ R/util.R
@@ -1,12 +1,20 @@
 ### Miscellaneous helpers ###
 
 
-# Helper function: Calculate vector length
+#' Calculate vector length
+#' 
+#' @param i_vect A vector
+#' @return The length of \code{i_vect}
+#' @example v_length(c(1,1,1,1))
 v_length <- function(i_vect) {
     sqrt(sum(i_vect^2))
 }
 
-# Normalize a vector to unit length
+#' Normalize a vector to unit length
+#' 
+#' @param i_vect A vector
+#' @return \code{i_vect} normalized to unit length
+#' @example v_normalize(c(1,1,1,1))
 v_normalize <- function(i_vect) {
     i_vect/v_length(i_vect)
 }
