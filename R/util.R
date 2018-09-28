15c16e33d6deea2fdccc0cc58d92a4de47e9a251
kpflugshaupt <kpflugshaupt@me.com>
2018-09-28 17:15:34 +0200

    cleanup

--- R/util.R
+++ R/util.R
@@ -1,10 +1,12 @@
-############################################################################################
-# Miscellaneous helpers
-############################################################################################
+### Miscellaneous helpers ###
 
 
 # Helper function: Calculate vector length
-v_length <- function(i_vect) { sqrt(sum(i_vect ^ 2)) }
+v_length <- function(i_vect) {
+    sqrt(sum(i_vect^2))
+}
 
 # Normalize a vector to unit length
-v_normalize <- function(i_vect) { i_vect / v_length(i_vect) }
+v_normalize <- function(i_vect) {
+    i_vect/v_length(i_vect)
+}
