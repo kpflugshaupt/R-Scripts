665687148af077a3c2e3456602c804a5c55fba61
kpflugshaupt <kpflugshaupt@me.com>
2018-09-28 16:45:49 +0200

    reformat

--- util.R
+++ util.R
@@ -4,7 +4,7 @@
 
 
 # Helper function: Calculate vector length
-v_length   <- function(i_vect) { sqrt(sum(i_vect ^ 2)) }
+v_length <- function(i_vect) { sqrt(sum(i_vect ^ 2)) }
 
 # Normalize a vector to unit length
-v_normalize <- function(i_vect) { i_vect / vlength(i_vect) }
+v_normalize <- function(i_vect) { i_vect / v_length(i_vect) }
